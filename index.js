25a76665e5db88b865b741fe87a5d88972911382
Ontaria Woods <ontaria.woods@gmail.com>
2022-10-26 11:18:38 -0400

    commit tree 190011a966b901bc66f5440d87eb9fbbda7b75f0

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution here
+const drivers = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+function findMatching(array, string) {
+  let names = array.filter(
+    (item) => item == string || item === string.toLowerCase()
+  );
+  return names;
+}
+
+function fuzzyMatch(drivers, string) {
+  let abbr = drivers.filter((item) => item.startsWith(string));
+  return abbr;
+}
+
+function matchName(drivers, string) {
+  let homeSweetHome = drivers.filter((item) => item.name === string);
+  return homeSweetHome;
+}
